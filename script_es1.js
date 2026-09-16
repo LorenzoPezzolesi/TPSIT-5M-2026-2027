4e22d7ceed2d57e48a0b5a138ff51b153ec71720
LorenzoPezzolesi <147714557+LorenzoPezzolesi@users.noreply.github.com>
2026-09-16 23:16:00 +0200

    es 1

---
+++ script_es1.js
@@ -0,0 +1,17 @@
+//1)In un tag h1 modificare tutti gli attributi
+const titolo = document.querySelector("h1");
+
+
+
+//2)Con id cambiare il colore
+
+
+
+
+//3)In un elenco: con una classe cambiare dimensione del font
+
+
+
+
+
+//4)In elenco con (query selector) tutti gli elementi siano visualizzati in grassetto
