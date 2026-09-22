4724ed56f199c7df51804ac2fbfbf26239c49e5a
LorenzoPezzolesi <147714557+LorenzoPezzolesi@users.noreply.github.com>
2026-09-22 23:18:08 +0200

    update

---
+++ Esercizi ripasso Javascrit 2/es2.js
@@ -0,0 +1,20 @@
+// 2) Trasforma le 4 voci della lista del punto 1) in link cliccabili
+
+// FUNZIONE 1:
+function trasformaLink() {
+
+}
+
+// 3) Modificare il testo di una voce dell'elenco prendendo i dati in input dall'utente (pop up)
+
+// FUNZIONE 2:
+function modificaVoce() {
+    // prompt chiede un dato all'utente è come l'input di python
+    let numero = prompt("Quale voce vuoi modificare? Scrivi un numero da 1 a 4", "1");
+
+    let nuovoTesto = prompt("Scrivi il nuovo testo per la voce modificata:", "");
+ 
+
+ 
+}
+ 
