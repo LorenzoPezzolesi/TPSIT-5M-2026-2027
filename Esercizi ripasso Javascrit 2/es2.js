f5163c5dfb1c1e0576ba5649927861a874b126d6
LorenzoPezzolesi <147714557+LorenzoPezzolesi@users.noreply.github.com>
2026-09-22 23:24:30 +0200

    update

--- Esercizi ripasso Javascrit 2/es2.js
+++ Esercizi ripasso Javascrit 2/es2.js
@@ -11,6 +11,12 @@ function trasformaLink() {
 function modificaVoce() {
     // prompt chiede un dato all'utente è come l'input di python
     let numero = prompt("Quale voce vuoi modificare? Scrivi un numero da 1 a 4", "1");
+    // L' "1" alla fine è il secondo parametro di prompt (è opzionale) ed è il valore predefinito 
+    // che appare già scritto nel campo di testo del popup quando si apre
+    // È utile per:
+    //- Suggerire un valore comune o ragionevole (in questo caso "1" suggerisce di modificare la prima voce)
+    //- Velocizzare l'utente, che può semplicemente premere OK se il valore di default va bene
+    //- Dare un esempio del formato atteso (es. mettere "aaaa-mm-gg" come default se chiedi una data)
 
     let nuovoTesto = prompt("Scrivi il nuovo testo per la voce modificata:", "");
  
