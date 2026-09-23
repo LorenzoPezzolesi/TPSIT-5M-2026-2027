97540062bb6d336a72efbc8405aed42caf35ba20
LorenzoPezzolesi <147714557+LorenzoPezzolesi@users.noreply.github.com>
2026-09-23 23:05:26 +0200

    update

--- Esercizi_ripasso2/es_1/es1.js
+++ Esercizi_ripasso2/es_1/es1.js
@@ -1,4 +1,6 @@
-
+//Chiedo all'utente il suo nome e lo salvo nella variabile "nome"
 let nome = prompt("Scrivi quale e il tuo nome", "nome");
 
+//Cerco il paragrafo con id "saluto" e cambio il suo testp com:
+//"Ciao" + il nome che ha inserito prima l'utente
 document.getElementById("saluto").textContent = "Ciao, " + nome;
